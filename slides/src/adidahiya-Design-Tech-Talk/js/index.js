897ddd9c697605b52ad288d71371feb615f83e46
Hasan Kumar Reddy A <mintuhouse@gmail.com>
2012-10-24 21:33:20 +0530

    Added basic set of slides

---
+++ slides/src/adidahiya-Design-Tech-Talk/js/index.js
@@ -0,0 +1,46 @@
+(function() {
+  'use strict';
+  /*global Reveal:true */
+
+  var TRANSITION  = 'default' // default/cube/page/concave/linear(2d)
+    , THEME       = 'simple'
+    , hasMarkdown = !!document.querySelector('[data-markdown]');
+
+  // Initialize presentation
+  Reveal.initialize({
+    controls: true        // controls in bottom right
+  , progress: true        // presentation progress bar
+  , history: true         // push each slide change to browser history
+  , keyboard: true        // allow keyboard nav
+  , loop: false           // loop presentation
+  , autoSlide: 0          // ms before auto-advance (0 to disable)
+  , mouseWheel: false     // mouse wheel nav
+  , rollingLinks: true    // apply 3D roll to link hover
+  , transition: TRANSITION
+  , theme: THEME
+  , dependencies: [
+      { // Syntax highlight for <code> elements
+        src: 'lib/js/highlight.js'
+      , async: true
+      , callback: function() {
+          window.hljs.initHighlightingOnLoad();
+        }
+      }
+    , { // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
+        src: 'lib/js/classList.js'
+      , condition: function() {
+          return !document.body.classList;
+        }
+      }
+    , { // Interpret Markdown in <section> elements
+        src: 'lib/js/data-markdown.js'
+      , condition: hasMarkdown
+      }
+    , { // Dependency for data-markdown.js
+        src: 'lib/js/showdown.js'
+      , condition: hasMarkdown
+      }
+    ]
+  });
+
+}).call(this);
---
+++ slides/src/adidahiya-Design-Tech-Talk/old/README.md
@@ -0,0 +1,12 @@
+Introduction to Web Design
+====================
+Making pretty web apps
+---------------------
+
+This talk covers:
+* Design 101 (color, layout, typography, contrast)
+* Getting Started with CSS (box model, selectors, grid system)
+* Advanced CSS (image sprites, pseudo classes, effects)
+* Odds & Ends (cross-browser support, boilerplates, CSS tools)
+
+...and assumes you know how to make a very simple HTML web page.
